a602f394613d43e112966fd6446d633c8062510c
Chris Jannenga <Cjannenga312@gmail.com>
2017-07-05 15:49:58 -0700

    Added image to greeting div

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 console.log("Javascript is alive!");
 
+// This is the Greeting text
+
 document.getElementById("greeting").innerHTML = "Hello, World!";
+
+// This changes the background color of all li tags
 
 var listItems = document.getElementsByTagName('li');
 for (i = 0; i < listItems.length; i++) {
   listItems[i].style.backgroundColor = "yellow";
 };
+
+// Create an img tag
+
+
+var img = document.createElement("img");
+img.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
+document.getElementById('greeting').appendChild(img);
