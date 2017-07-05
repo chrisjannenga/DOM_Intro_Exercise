191571c0fdfa9536e6f13647155bfc832c5edf06
Chris Jannenga <Cjannenga312@gmail.com>
2017-07-05 14:53:27 -0700

    Changed the BG Color of the List items

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 console.log("Javascript is alive!");
 
 document.getElementById("greeting").innerHTML = "Hello, World!";
+
+var listItems = document.getElementsByTagName('li');
+for (i = 0; i < listItems.length; i++) {
+  listItems[i].style.backgroundColor = "yellow";
+};
