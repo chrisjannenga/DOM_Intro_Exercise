37febf6ce9092ec3431a73f79ec1d84bc2ec077b
Chris Jannenga <Cjannenga312@gmail.com>
2017-07-06 13:36:37 -0700

    Fixed Task 1, also finished task 3 and 4

--- DOM_jQuery_intro/app.js
+++ DOM_jQuery_intro/app.js
@@ -1,5 +1,21 @@
 console.log("Javascript is alive!");
 
-$("#greeting").replaceWith("Hello, World!");
+// Task 1
+
+$("#greeting").html("Hello, World!");
+
+// Task 2
 
 $("li").css('backgroundColor', 'yellow');
+
+// Task 3
+
+$("#greeting").append('<img src="http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif" alt=" Waving Gif" />');
+
+// Task 4
+
+$("li").click(function() {
+  $(this).toggleClass("selected");
+});
+
+// Task 5
